4102d311d82a59e356c6d390335d38178d56dc50
CharlesTaylor7 <charlestaylor95@gmail.com>
2021-07-11 22:05:21 -0400

    Svgs for the grid

--- components/Grid.tsx
+++ components/Grid.tsx
@@ -5,8 +5,27 @@ import { Route } from '@/types'
 
 function Grid({ map = { 23: {code: 2} }}) {
     return (
-        <div className={styles.grid}>
-            {Array.from({length: 49}, (_, i) => (<Cell key={i} route={map[i]}/>))}
+        <div className={styles.gridRow}>
+        <svg
+            className={styles.grid}
+            viewBox="-0.5 -0.5 7.5 7.5"
+        >
+            // horizontal rows
+            {Array.from({length: 8}, (_, i) => (
+                <line key={i} className={styles.gridLine}
+                    strokeWidth={0.01}
+                    x1="0" x2="7"
+                    y1={i} y2={i}
+                />)
+            )}
+            {Array.from({length: 8}, (_, i) => (
+                <line key={i} className={styles.gridLine}
+                    strokeWidth={0.01}
+                    y1="0" y2="7"
+                    x1={i} x2={i}
+                />)
+            )}
+        </svg>
         </div>
     )
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,18 +1,17 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import Grid from '@/components/Grid.tsx'
-import styles from '@/styles/Home.module.css'
 
 export default function Home() {
     return (
-        <div className={styles.container}>
+        <>
             <Head>
                 <title>Railroad Inc. AI</title>
                 <link rel="icon" href="/favicon.ico" />
             </Head>
-            <main className={styles.main}>
+            <main>
                 <Grid/>
             </main>
-        </div>
+        </>
     )
 }
