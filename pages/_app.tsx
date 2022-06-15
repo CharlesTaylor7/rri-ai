919fb0024b3918e3c8b7cc5ada97d234bf84218f
Charles Taylor <charlestaylor95@gmail.com>
2022-06-15 10:27:50 -0400

    WIP

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,20 +1,12 @@
 import type { AppProps } from "next/app";
 import Head from "next/head";
-import {useCallback, useState} from "react";
 import Error from "components/Error";
-import { Provider } from 'app/context'
-import {GameState} from "app/server/state";
 
 export default function App({ Component, pageProps }: AppProps) {
-  console.log(pageProps)
-  const { error, state: initialState, ...rest } = pageProps;
+  const { error, ...rest } = pageProps;
   if (error) {
     return <Error {...error} />;
   }
-
-  const [ state, setState] = useState<GameState>(initialState)
-  const pushState = useCallback((updates: Partial<GameState>) => setState(state => ({...state, ...updates})), [setState])
-
   return (
     <>
       <Head>
@@ -22,9 +14,7 @@ export default function App({ Component, pageProps }: AppProps) {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main>
-        <Provider value={{state, pushState}}>
-          <Component {...rest} />
-        </Provider>
+        <Component {...rest} />
       </main>
     </>
   );
--- pages/game.tsx
+++ pages/game.tsx
@@ -1,11 +1,31 @@
+import {useState, useCallback} from "react"
 import Grid from "components/game/Grid";
 import Dice from "components/game/Dice";
 import DiceButton from "components/game/DiceButton";
 import styles from "styles/Game.module.css";
 import debugData from "app/debugData";
+import { Provider } from 'app/context'
+import { GameState } from 'app/server/state'
+import {RouteInfo} from "app/types";
 
-export default function Game() {
+type Props = {
+  state: {
+    game: {
+      routes: {
+        current: Array<RouteInfo>,
+        pending: Array<RouteInfo>,
+      }, 
+      diceCodes: Array<number>
+    }
+  }
+}
+
+export default function Game(props: Props) {
+  const [ state, setState ] = useState<GameState>(props.state)
+  const pushState = useCallback((updates: Partial<GameState>) => setState(state => ({...state, ...updates})), [setState])
+
   return (
+    <Provider value={{state, pushState}}>
     <div className={styles.gameRow}>
       <Grid />
       <div className={styles.rightPanel}>
@@ -13,6 +33,7 @@ export default function Game() {
         <Dice />
       </div>
     </div>
+    </Provider>
   );
 }
 
